f5d96246dff1a0effb3b3109dcdd4c707f12a55a
nn80nn <nn80nn80nn80nn80nn@gmail.com>
2023-10-12 16:54:43 +0300

    Initial commit Sem2

---
+++ src/Sem2/Solution2_18.java
@@ -0,0 +1,27 @@
+package Sem2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2_18 {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        // Добавление пар строк в коллекцию
+        map.put("арбуз", "ягода");
+        map.put("банан", "трава");
+        map.put("вишня", "ягода");
+        map.put("груша", "фрукт");
+        map.put("дыня", "овощ");
+        map.put("ежевика", "куст");
+        map.put("жень-шень", "корень");
+        map.put("земляника", "ягода");
+        map.put("ирис", "цветок");
+        map.put("картофель", "клубень");
+
+        // Вывод содержимого коллекции
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
