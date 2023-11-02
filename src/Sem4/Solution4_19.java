238b78753f52338d2e694d1d7e46795a6503a11c
nn80nn <nn80nn80nn80nn80nn@gmail.com>
2023-11-02 18:13:50 +0300

    Initial commit Sem4

---
+++ src/Sem4/Solution4_19.java
@@ -0,0 +1,75 @@
+package Sem4;
+
+import java.util.Scanner;
+
+public class Solution4_19 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            Cat cat = CatFactory.getCatByKey(input);
+            System.out.println(cat.toString());
+        }
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat;
+            switch (key) {
+                case "vaska":
+                    cat = new MaleCat("Василий");
+                    break;
+                case "murka":
+                    cat = new FemaleCat("Мурочка");
+                    break;
+                case "kiska":
+                    cat = new FemaleCat("Кисюлька");
+                    break;
+                default:
+                    cat = new Cat(key);
+                    break;
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "Я уличный кот " + getName();
+        }
+    }
+
+    static class MaleCat extends Cat {
+        MaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - солидный кошак по имени " + getName();
+        }
+    }
+
+    static class FemaleCat extends Cat {
+        FemaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - милая кошечка по имени " + getName();
+        }
+    }
+}
